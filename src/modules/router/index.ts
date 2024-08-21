1217bddba2af875227b0cf9c840b291a83cb0136
Elsa-Cotrina <elsacotrina58@gmail.com>
2024-08-21 18:07:10 -0500

    se acepto la fusion de sidebar

--- src/modules/router/index.ts
+++ src/modules/router/index.ts
@@ -15,7 +15,7 @@ export const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('@/modules/views/SidebarLogin.vue')
+      component: () => import('@/modules/views/SideBarView.vue')
     }
   ]
 })
--- src/modules/views/SidebarLogin.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <h1>sidebar login</h1>
-</template>
-
-<script lang="ts" setup>
-
-</script>
